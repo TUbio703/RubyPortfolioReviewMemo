0917a05d5dc229ac12a4730a15f0c5c0dfdc19f8
Taiga <sokuratesu64@gmail.com>
2026-06-24 14:31:35 +0900

    fix: fix test fixtures and helpers

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class EntriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @entry = entries(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test "should get index" do
@@ -17,10 +19,9 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create entry" do
     assert_difference("Entry.count") do
-      post entries_url, params: { entry: { body: @entry.body, media_type: @entry.media_type, rating: @entry.rating, title: @entry.title } }
+      post entries_url, params: { entry: { body: @entry.body, rating: @entry.rating, title: @entry.title } }
     end
-
-    assert_redirected_to entry_url(Entry.last)
+    assert_redirected_to root_path
   end
 
   test "should show entry" do
@@ -34,15 +35,14 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update entry" do
-    patch entry_url(@entry), params: { entry: { body: @entry.body, media_type: @entry.media_type, rating: @entry.rating, title: @entry.title } }
-    assert_redirected_to entry_url(@entry)
+    patch entry_url(@entry), params: { entry: { body: @entry.body, rating: @entry.rating, title: @entry.title } }
+    assert_redirected_to root_path
   end
 
   test "should destroy entry" do
     assert_difference("Entry.count", -1) do
       delete entry_url(@entry)
     end
-
-    assert_redirected_to entries_url
+    assert_redirected_to root_path
   end
 end
