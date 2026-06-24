bccfa89db3f24570593f08b81b5ba1f085a4b93d
Taiga <sokuratesu64@gmail.com>
2026-06-24 14:36:56 +0900

    Fix duplicate association in EntryTag model

--- app/models/entry_tag.rb
+++ app/models/entry_tag.rb
@@ -1,7 +1,6 @@
 class EntryTag < ApplicationRecord
   belongs_to :entry
   belongs_to :tag
-    has_many :entry_tags, dependent: :destroy
-  has_many :tags, through: :entry_tags
+
   has_many :likes, dependent: :destroy
 end
